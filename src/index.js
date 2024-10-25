e23f9151ff31ce72370700e6db7740194c040f59
EiEiKyaw <11eikyw@gmail.com>
2024-10-26 02:39:24 +0630

    update js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function showInfo(event){
-    event.preventDefault();
-    let city = document.querySelector("#input-box");
-    let cityElement = document.querySelector("#city-display");
-    cityElement.innerHTML = city.value;
+function showTemp(response){
+    let tempElement = document.querySelector("#degree");
+    let temp = response.data.temperature.current;
+    tempElement.innerHTML = Math.round(temp.value);
+    console.log(temp);
+}
+
+function searchCity(city) {
+  let apiKey = "736746t37o0a4075f6772e4f9dc8291b";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(url).then(showTemp);
+}
+
+function searchInfo(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#input-box");
+  let cityElement = document.querySelector("#city-display");
+  cityElement.innerHTML = searchCity.value;
+  searchCity(searchCity.value);
 }
 
 let searchBtnElement = document.querySelector("#search-form");
-searchBtnElement.addEventListener("submit", showInfo);
+searchBtnElement.addEventListener("submit", searchInfo);
